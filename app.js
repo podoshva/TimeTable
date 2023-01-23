c630f9499527c7cadafa8bad364cca0437077899
podoshva <108821023+podoshva@users.noreply.github.com>
2023-01-23 12:06:18 +0500

    3rd commit

--- app.js
+++ app.js
@@ -2,7 +2,38 @@
 
 const Monday = {
   "day": "Понедельник",
-  "lessons": ["Рус.яз.", "Химия", "Истор.", "Алгеб.", "Литер.", "Физика",],
+  "lessons": [
+    {
+      lesson: "Рус.яз.",
+      time: "13:00–13:40",
+      timeEnd: 820,
+    },
+    {
+      lesson: "Химия",
+      time: "14:00–14:40",
+      timeEnd: 880,
+    },
+    {
+      lesson: "Истор.",
+      time: "14:55–15:35",
+      timeEnd: 935,
+    },
+    {
+      lesson: "Алгеб.",
+      time: "15:50–16:30",
+      timeEnd: 990,
+    },
+    {
+      lesson: "Литер.",
+      time: "16:40–17:20",
+      timeEnd: 1040,
+    },
+    {
+      lesson: "Физика",
+      time: "17:30–18:10",
+      timeEnd: 1090,
+    }
+  ]
 }
 
 const Teusday = {
@@ -30,12 +61,20 @@ function getDay() {
   return currentDate.getDay() - 1;
 }
 
+function getHours() {
+  const currentDate = new Date();
+  return currentDate.getHours() * 60 + currentDate.getMinutes();
+}
+
 function timeTable(day) {
   const lessons = document.querySelector(".lessons");
   for(let i = 0; i < days[day].lessons.length; i++) {
     const lesson = document.createElement('div');
     lesson.setAttribute("class", "lesson")
-    lesson.innerHTML = days[day].lessons[i];
+    lesson.innerHTML = days[day].lessons[i].lesson;
+    if(getHours() > days[day].lessons[i].timeEnd) {
+      lesson.setAttribute("style", "background-color: rgb(73, 207, 73);")
+    }
     if(i == days[day].lessons.length - 1) {
       lesson.setAttribute("style", "border: none;")
     }
