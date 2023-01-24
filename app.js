5385d0f9b150e0c536f75d2d94faf4ca19ed186a
podoshva <108821023+podoshva@users.noreply.github.com>
2023-01-24 13:31:59 +0500

    Big update
    1) Other days added
    2) Redesigned
    3) Optimised code

--- app.js
+++ app.js
@@ -4,56 +4,182 @@ const Monday = {
   "day": "Понедельник",
   "lessons": [
     {
-      lesson: "Рус.яз.",
+      lesson: "Русск.яз",
       time: "13:00–13:40",
-      timeEnd: 820,
     },
     {
       lesson: "Химия",
       time: "14:00–14:40",
-      timeEnd: 880,
     },
     {
       lesson: "Истор.",
       time: "14:55–15:35",
-      timeEnd: 935,
     },
     {
       lesson: "Алгеб.",
       time: "15:50–16:30",
-      timeEnd: 990,
     },
     {
       lesson: "Литер.",
       time: "16:40–17:20",
-      timeEnd: 1040,
     },
     {
       lesson: "Физика",
       time: "17:30–18:10",
-      timeEnd: 1090,
-    }
+    },
   ]
 }
 
 const Teusday = {
   "day": "Вторник",
-  "lessons": ["Биол.", "Литер.", "Физ.ра", "Геогр.", "Геом.", "Ан.", "Пр.мат",],
+  "lessons": [
+    {
+      lesson: "-",
+      time: "12:15–12:55",
+    },
+    {
+      lesson: "Биолог.",
+      time: "13:00–13:40",
+    },
+    {
+      lesson: "Литер.",
+      time: "14:00–14:40",
+    },
+    {
+      lesson: "Физ-ра",
+      time: "14:55–15:35",
+    },
+    {
+      lesson: "Геогр.",
+      time: "15:50–16:30",
+    },
+    {
+      lesson: "Геом.",
+      time: "16:40–17:20",
+    },
+    {
+      lesson: "Англ.яз",
+      time: "17:30–18:10",
+    },
+    {
+      lesson: "Практ.мат",
+      time: "18:20-19:00",
+    }
+  ]
 }
 
 const Wednesday = {
   "day": "Среда",
-  "lessons": ["Ан.", "Алгеб.", "Инф.", "Tex.", "Рус.яз", "Физ-ра", "Геогр.",],
+  "lessons": [
+    {
+      lesson: "Геогр.",
+      time: "12:15–12:55",
+    },
+    {
+      lesson: "Англ.яз",
+      time: "13:00–13:40",
+    },
+    {
+      lesson: "Алгеб.",
+      time: "14:00–14:40",
+    },
+    {
+      lesson: "Информ.",
+      time: "14:55–15:35",
+    },
+    {
+      lesson: "Техн.",
+      time: "15:50–16:30",
+    },
+    {
+      lesson: "Русск.яз",
+      time: "16:40–17:20",
+    },
+    {
+      lesson: "Физ-ра",
+      time: "17:30–18:10",
+    },
+    {
+      lesson: "-",
+      time: "18:20-19:00",
+    },
+  ] 
 }
 
 const Thursday = {
   "day": "Четверг",
-  "lessons": ["Физ-ра", "Геом.", "Рус.яз", "Общ-во", "Физика", "Химия",],
+  "lessons": [
+    {
+      lesson: "-",
+      time: "12:15–12:55",
+    },
+    {
+      lesson: "Физ-ра",
+      time: "13:00–13:40",
+    },
+    {
+      lesson: "Геом.",
+      time: "14:00–14:40",
+    },
+    {
+      lesson: "Русск.яз",
+      time: "14:55–15:35",
+    },
+    {
+      lesson: "Общ-во",
+      time: "15:50–16:30",
+    },
+    {
+      lesson: "Физика",
+      time: "16:40–17:20",
+    },
+    {
+      lesson: "Химия",
+      time: "17:30–18:10",
+    },
+    {
+      lesson: "-",
+      time: "18:20-19:00",
+    },
+  ],
 }
 
 const Friday = {
   "day": "Пятница",
-  "lessons": ["ОБЖ", "Истор.", "Биол.", "Алгеб.", "Род.лит.", "Ан.", "Иск-во"],
+  "lessons": [
+    {
+      lesson: "Иск-во",
+      time: "12:15–12:55",
+    },
+    {
+      lesson: "ОБЖ",
+      time: "13:00–13:40",
+    },
+    {
+      lesson: "Истор.",
+      time: "14:00–14:40",
+    },
+    {
+      lesson: "Биолог.",
+      time: "14:55–15:35",
+    },
+    {
+      lesson: "Алгеб.",
+      time: "15:50–16:30",
+    },
+    {
+      lesson: "Род.лит",
+      time: "16:40–17:20",
+    },
+    {
+      lesson: "Англ.яз",
+      time: "17:30–18:10",
+    },
+    {
+      lesson: "-",
+      time: "18:20-19:00",
+    },
+  ],
 }
 
 function getDay() {
@@ -70,23 +196,53 @@ function timeTable(day) {
   const lessons = document.querySelector(".lessons");
   for(let i = 0; i < days[day].lessons.length; i++) {
     const lesson = document.createElement('div');
-    lesson.setAttribute("class", "lesson")
+    lesson.setAttribute("class", "lesson");
     lesson.innerHTML = days[day].lessons[i].lesson;
-    if(getHours() > days[day].lessons[i].timeEnd) {
+<<<<<<< HEAD
+    let lessonTimeEnd = 0;
+    console.log(i);
+    switch(i) {
+      case 0:
+        lessonTimeEnd = 775;
+        break;
+      case 1:
+        lessonTimeEnd = 820;
+        break;
+      case 2:
+        lessonTimeEnd = 880;
+        break;
+      case 3:
+        lessonTimeEnd = 935;
+        break;
+      case 4:
+        lessonTimeEnd = 990;
+        break;
+      case 5:
+        lessonTimeEnd = 1040;
+        break;
+      case 6:
+        lessonTimeEnd = 1090;
+        break;
+      case 7:
+        lessonTimeEnd = 1140;
+        break;
+      default:
+        break;
+    if(getHours() >= days[day].lessons[i].timeEnd) {
       lesson.setAttribute("style", "background-color: rgb(73, 207, 73);")
     }
-    if(i == days[day].lessons.length - 1) {
-      lesson.setAttribute("style", "border: none;")
+    if(getHours() >= lessonTimeEnd) {
+      lesson.setAttribute("style", "background-color: rgb(142, 212, 142);");
     }
     lessons.appendChild(lesson);
   }
 }
-
 const days = [Monday, Teusday, Wednesday, Thursday, Friday];
 const day = getDay();
 
 if(day == -1 || day == 5) {
   alert("Сегодня выходной");
 } else {
+  alert("Эксклюзивное расписание для 8Д❤️\nОно обновляется автоматически каждый день");
   timeTable(day);
 }
